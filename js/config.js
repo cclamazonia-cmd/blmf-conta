c96357139f17e509fcb0b5eff42c19067c3780e8
cclamazonia-cmd <cclamazonia@gmail.com>
2026-03-03 12:12:51 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  SUPABASE_URL: "https://EXEMPLE.supabase.co",
+  SUPABASE_ANON_KEY: "EXEMPLE_ANON_KEY",
+  FUNCTIONS_BASE_URL: "https://EXEMPLE.supabase.co/functions/v1"
+};
