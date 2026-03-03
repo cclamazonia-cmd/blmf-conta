afcb92ce521dda08bb516377c69f646bf44f3c11
cclamazonia-cmd <cclamazonia@gmail.com>
2026-03-03 13:13:09 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  SUPABASE_URL: https://asptfmokzykwzlshsncw.supabase.co,
-  SUPABASE_ANON_KEY: sb_publishable_38cFV9vsGhmqMp-Z5gmV4w_HY6ReWSF,
-  FUNCTIONS_BASE_URL: https://asptfmokzykwzlshsncw.supabase.co/functions/v1
+  SUPABASE_URL: "https://asptfmokzykwzlshsncw.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_38cFV9vsGhmqMp-Z5gmV4w_HY6ReWSF",
+  FUNCTIONS_BASE_URL: "https://asptfmokzykwzlshsncw.supabase.co/functions/v1"
 };
